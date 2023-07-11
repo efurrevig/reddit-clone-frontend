25fb1db153ad354a19e105c147a27357149b287d
Eric Furrevig <efurrevig@gmail.com>
2023-07-11 18:12:40 -0400

    More post form progress

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from 'react'
 import { Icons } from './Icons'
+import Button from './Button'
 
 const PostForm = () => {
 
@@ -14,17 +15,48 @@ const PostForm = () => {
         console.log('form submitted:', form)
     }
 
+    const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newTitle = e.target.value.slice(0, 300)
+        setForm({
+            ...form,
+            title: newTitle
+        });
+    }
+
+    const handleBodyChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setForm({
+            ...form,
+            body: e.target.value
+        })
+    }
+
     return (
-        <div className='flex flex-col rounded bg-gray-900'>
-            <div className='flex flex-1 bg-gray-900'>
-                <form onSubmit={handleFormSubmit}>
-                    <input
-                        className='bg-gray-900 border border-gray-700 rounded p-2 w-96 text-sm'
-                        type='text'
+        <div className='flex flex-col rounded w-full bg-gray-900'>
+            <div className='flex flex-col bg-gray-900 m-2 gap-2'>
+                <form 
+                    className='flex flex-col gap-2 p-2 pb-0'
+                    onSubmit={handleFormSubmit}
+                >
+                    <textarea
+                        className='bg-gray-900 w-full h-10 overflow-x-hidden break-words border border-gray-700 rounded p-2 text-sm'
                         placeholder='Title'
                         value={form.title}
+                        onChange={handleTitleChange}
+                        required={true}
                     >
-                    </input>
+                    </textarea>
+                    <textarea
+                        className='overflow-hidden break-words h-32 bg-gray-900 w-full border border-gray-700 rounded p-2 text-sm'
+                        placeholder='Text (optional)'
+                        value={form.body}
+                        onChange={handleBodyChange}
+                    >
+                    </textarea>
+                    <div className='flex flex-row justify-end py-1'>
+                        <Button clearDefault={true} customClass='bg-cyan-900 text-white font-bold py-1 px-4 rounded-3xl flex justify-center items-center gap-2'>
+                            Post
+                        </Button>
+                    </div>
                 </form>
             </div>
         </div>

55514e6fd1e9396e0ccb434a205c37a1c016224c
Eric Furrevig <efurrevig@gmail.com>
2023-07-11 20:28:54 -0400

    Post type buttons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -107,8 +107,8 @@ export const Icons = {
     ),
     link: (props: LucideProps) => (
         <svg xmlns="http://www.w3.org/2000/svg"
-            width="24" 
-            height="24" 
+            width={props.width || "24"} 
+            height={props.width || "24"} 
             viewBox="0 0 24 24" 
             fill="none" 
             stroke="currentColor" 
@@ -199,6 +199,24 @@ export const Icons = {
             className="lucide lucide-message-square"
         >
             <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+        </svg>
+    ),
+    post: (props: LucideProps) => (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width={props.width || "18"} 
+            height={props.height || "18"} 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            className="lucide lucide-menu-square"
+        >
+            <rect width="18" height="18" x="3" y="3" rx="2"/>
+            <path d="M7 8h10"/>
+            <path d="M7 12h10"/>
+            <path d="M7 16h10"/>
         </svg>
     ),
 }
--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -4,7 +4,7 @@ import { Icons } from './Icons'
 import Button from './Button'
 
 const PostForm = () => {
-
+    const [postType, setPostType] = useState<'text' | 'link'>('text')
     const [form, setForm] = useState({
         title: '',
         body: '',
@@ -12,6 +12,7 @@ const PostForm = () => {
 
     const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        console.log(e)
         console.log('form submitted:', form)
     }
 
@@ -30,13 +31,41 @@ const PostForm = () => {
         })
     }
 
+    const handleTypeChange = (type: 'text' | 'link') => {
+        setPostType(type)
+    }
+
     return (
         <div className='flex flex-col rounded w-full bg-gray-900'>
+            <div className='flex flex-row w-full border-b border-gray-700'>
+                <Button 
+                    clearDefault={true} 
+                    customClass={`flex w-1/2 items-center justify-center h-10 gap-2 border-r border-r-gray-700 ${postType === 'text' ? 'border-b-2 border-b-cyan-700' : 'border-gray-700'}`}
+                    onClick={() => handleTypeChange('text')}
+                >
+                    <Icons.post />
+                    Post
+                </Button>
+                <Button 
+                    clearDefault={true} 
+                    customClass={`flex w-1/2 justify-center h-10 items-center gap-2 ${postType === 'link' ? 'border-b-2 border-cyan-700' : 'border-gray-700'}`}
+                    onClick={() => handleTypeChange('link')}
+                >
+                    <Icons.link height='18' width='18' />
+                    Link
+                </Button>
+
+            </div>
             <div className='flex flex-col bg-gray-900 m-2 gap-2'>
                 <form 
                     className='flex flex-col gap-2 p-2 pb-0'
                     onSubmit={handleFormSubmit}
                 >
+                    <input
+                        type='hidden'
+                        value={postType}
+                    >
+                    </input>
                     <textarea
                         className='bg-gray-900 w-full h-10 overflow-x-hidden break-words border border-gray-700 rounded p-2 text-sm'
                         placeholder='Title'

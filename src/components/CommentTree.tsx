248a33c61987beeefce0bfeb84fdd1790d787307
Eric Furrevig <efurrevig@gmail.com>
2023-07-28 18:28:59 -0400

    No comment display

--- src/components/CommentTree.tsx
+++ src/components/CommentTree.tsx
@@ -3,6 +3,7 @@ import { Comment } from "@/types"
 import { useState } from "react"
 import CommentDisplay from "./CommentDisplay"
 import CommentForm from "./CommentForm"
+import { Icons } from "./Icons"
 
 const CommentTree = ({ comments, post_id } : {comments: Comment[], post_id: number}) => {
     const [newUserComments, setNewUserComments] = useState<Comment[]>([])
@@ -15,7 +16,13 @@ const CommentTree = ({ comments, post_id } : {comments: Comment[], post_id: numb
                 newUserComments={newUserComments}
                 setNewUserComments={setNewUserComments}
             />
-            <div className="mx-2">
+            <div className="mx-2 mb-4">
+                {comments.length === 0 && newUserComments.length === 0 && 
+                    <div className="text-gray-600 flex flex-col items-center justify-center h-72">
+                        <Icons.comments />
+                        <span>No comments yet</span>
+                    </div>
+                }
                 {newUserComments.map((c) => {
                     return (
                         <CommentDisplay key={c.id} comment={c} />

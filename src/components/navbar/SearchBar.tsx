2a1cd737d3fdd2e35cee0830a5a656ac64e15c08
Eric Furrevig <efurrevig@gmail.com>
2023-07-30 16:08:35 -0400

    Searchbar results only show on input focus

--- src/components/navbar/SearchBar.tsx
+++ src/components/navbar/SearchBar.tsx
@@ -8,6 +8,7 @@ import { Icons } from '../Icons'
 const SearchBar = () => {
     const [query, setQuery] = useState<string>('')
     const [searchResults, setSearchResults] = useState<Community[]>([])
+    const [showSearchResults, setShowSearchResults] = useState<boolean>(false)
 
     useEffect(() => {
 
@@ -17,7 +18,6 @@ const SearchBar = () => {
                 setSearchResults(res)
             } catch (error) {
                 setSearchResults([])
-                console.log(error)
             }
         }
         if (query.length > 0) {
@@ -28,7 +28,6 @@ const SearchBar = () => {
 
     }, [query])
 
-    console.log(searchResults)
 
     return (
         <div className="relative flex items-center h-full w-full">
@@ -36,33 +35,35 @@ const SearchBar = () => {
                 className="bg-gray-700 py-2 px-3 rounded-2xl w-full outline-none focus:ring-1 focus:ring-white focus:ring-opacity-50 text-sm text-gray-100"
                 type="text" 
                 placeholder="Search"
+                onFocus={() => setShowSearchResults(true)}
+                onBlur={() => setShowSearchResults(false)}
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
 
             />
-            <div className="absolute bg-gray-900 top-full gap-1 mb-2 flex flex-col w-full -mt-1 border border-t-0 border-slate-500">
-                {searchResults.length === 0 ? (
-                    <p className="p-4">No results found</p>
-                ) : (
-                    <div>
-                        {searchResults.map((community) => {
-                            return (
-                                <Link
-                                    key={community.id}
-                                    href={`/c/${community.id}/${community.name}`}
-                                    prefetch={false}
-                                    className="flex flex-row gap-1 py-1 px-4 hover-bg-gray-700 items-center"
-                                >
-                                    <Icons.logo />
-                                    <p>c/{community.name}</p>
-                                </Link>
+            {showSearchResults && (
+              <div className="absolute bg-gray-900 top-full gap-1 mb-2 flex flex-col w-full -mt-1 border border-t-0 border-slate-500">
+                    {searchResults.length === 0 ? (
+                        <p className="p-4 text-gray-400 text-sm text-center">No results found</p>
+                    ) : (
+                        <div>
+                            {searchResults.map((community) => {
+                                return (
+                                    <Link
+                                        key={community.id}
+                                        href={`/c/${community.id}/${community.name}`}
+                                        prefetch={false}
+                                        className="flex flex-row gap-1 py-1 px-4 hover-bg-gray-700 items-center"
+                                    >
+                                        <Icons.logo />
+                                        <p>c/{community.name}</p>
+                                    </Link>
+                                )}
                             )}
-                        )}
-                    </div>
-                )}
-                
-
-            </div>
+                        </div>
+                    )}
+                </div>  
+            )}
         </div>
     )
 }
